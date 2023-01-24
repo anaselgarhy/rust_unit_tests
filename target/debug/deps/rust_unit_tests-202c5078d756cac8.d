7762be89f4de12abf5ae5f3ee064fd09faa6846d
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-01-24 18:00:48 +0200

    Delete target

--- target/debug/deps/rust_unit_tests-202c5078d756cac8.d
+++
@@ -1,6 +0,0 @@
-/mnt/Data/Learning/Rust/rust_unit_tests/target/debug/deps/rust_unit_tests-202c5078d756cac8: src/main.rs src/super_villain.rs
-
-/mnt/Data/Learning/Rust/rust_unit_tests/target/debug/deps/rust_unit_tests-202c5078d756cac8.d: src/main.rs src/super_villain.rs
-
-src/main.rs:
-src/super_villain.rs:
--- target/debug/rust_unit_tests.d
+++
@@ -1,1 +0,0 @@
-/mnt/Data/Learning/Rust/rust_unit_tests/target/debug/rust_unit_tests: /mnt/Data/Learning/Rust/rust_unit_tests/src/main.rs /mnt/Data/Learning/Rust/rust_unit_tests/src/super_villain.rs
